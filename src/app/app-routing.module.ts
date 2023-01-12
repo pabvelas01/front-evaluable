1a17c10ff954bfbed73f4192c6058aeabcad9f7d
Pablo Velasquez <pvelasquez@ubiobio.cl>
2023-01-12 15:42:20 -0300

    Agrega componetes login y principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministradorComponent } from './components/administrador/administrador.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = 
 [
-  { path: 'first', component: LoginComponent },
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'administrador', component: AdministradorComponent },
+  { path: '**', redirectTo:'', pathMatch:'full' }, /*** Para retornar a la principal cuando url no hace match con nada */
 ];
 
 @NgModule({
